9dbce04b0bb9e6938becb8e014f768f7018792cf
Dev-kabingu <missmercykabingu@gmail.com>
2025-04-17 00:19:41 +0300

    update

--- backend/index.js
+++ backend/index.js
@@ -65,4 +65,4 @@ app.get("/api/message", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(` Server running on port ${PORT}`));
